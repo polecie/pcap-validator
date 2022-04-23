355608f7cd00ae3e963d34d67ca6da5844f59ced
Deckardio <47296872+Deckardio@users.noreply.github.com>
2022-04-23 13:37:28 +1000

    hot fix

--- analyze_pcap.py
+++ analyze_pcap.py
@@ -20,8 +20,7 @@ def summary_data(file_name):
         app_bytes = (streamer.groupby(['application_name','application_category_name'],as_index=False)['bidirectional_bytes'].sum())
         streamer_info = streamer[col_list].drop_duplicates(['src_ip', 'dst_ip', 'application_name'])
         return app_bytes, streamer_info
-    else:
-        return "there's no information about VPN"
+    return "there's no information about VPN или все нае******"
         
 if __name__ == "__main__":
     print(summary_data('files\ipsec.pcap'))
